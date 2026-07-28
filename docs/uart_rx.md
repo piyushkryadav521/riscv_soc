3856aa60c874168e7c6ecfcdcdaa9d57dd85ae33
Piyush Kumar Yadav <piyush.kr.yadav521@gmail.com>
2026-07-28 19:12:39 +0000

    Implemented UART Reciever RTL

---
+++ docs/uart_rx.md
@@ -0,0 +1,8 @@
+
+UART Receiver RTL
+
+Features:
+- Start bit detection
+- Serial-to-parallel conversion
+- Configurable baud rate
+- rx_done pulse generation
---
+++ rtl_ip/uart/uart_rx.v
@@ -0,0 +1,97 @@
+module uart_rx #(
+    parameter CLK_FREQ  = 50000000,
+    parameter BAUD_RATE = 115200
+)(
+    input clk,
+    input rst,
+    input rx,
+
+    output reg [7:0] rx_data,
+    output reg rx_done
+);
+
+localparam integer BAUD_COUNT = CLK_FREQ / BAUD_RATE;
+
+integer baud_counter;
+reg [2:0] bit_counter;
+reg [7:0] shift_reg;
+
+reg receiving;
+
+always @(posedge clk or posedge rst)
+begin
+
+    if(rst)
+    begin
+
+        baud_counter <= 0;
+        bit_counter <= 0;
+
+        receiving <= 0;
+
+        rx_done <= 0;
+
+        rx_data <= 0;
+        shift_reg <= 0;
+
+    end
+
+    else
+    begin
+
+        rx_done <= 0;
+
+        if(!receiving)
+        begin
+
+            if(rx==0)
+            begin
+
+                receiving <= 1;
+
+                baud_counter <= 0;
+
+                bit_counter <= 0;
+
+            end
+
+        end
+
+        else
+        begin
+
+            if(baud_counter == BAUD_COUNT-1)
+            begin
+
+                baud_counter <= 0;
+
+                if(bit_counter <3'd7)
+                begin
+
+                    shift_reg[bit_counter] <= rx;
+                    bit_counter <= bit_counter + 1;
+
+                end
+
+                else
+                begin
+                    shift_reg[bit_counter] <= rx;
+                    receiving <= 0;
+                    rx_data <= shift_reg;
+                    rx_done <= 1;
+
+                end
+
+            end
+
+            else
+
+                baud_counter <= baud_counter + 1;
+
+        end
+
+    end
+
+end
+
+endmodule
